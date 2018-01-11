34943f88aa02b2c4144beee136d6a4c669f594e3
Rob DeRosa <rob.derosa@xamarin.com>
2018-01-11 15:26:17 -0700

    Directory changes

---
+++ src/CustomVisionHack/StartHere/MyMobileApp.Common/Views/MainPage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace MyMobileApp.Common
+{
+	public class MainPage : TabbedPage
+	{
+		public MainPage()
+		{
+			var identifyPage = new NavigationPage(new PredictionDetailsPage())
+			{
+				Title = "Predict"
+			};
+
+			var itemsPage = new NavigationPage(new PredictionListPage())
+			{
+				Title = "History"
+			};
+
+			var settingsPage = new NavigationPage(new SettingsPage())
+			{
+				Title = "Settings"
+			};
+
+			switch(Device.RuntimePlatform)
+			{
+				case Device.iOS:
+					itemsPage.Icon = "tab_feed.png";
+					identifyPage.Icon = "tab_about.png";
+					settingsPage.Icon = "tab_about.png";
+					break;
+			}
+
+			Children.Add(identifyPage);
+			Children.Add(itemsPage);
+			Children.Add(settingsPage);
+
+			Title = Children[0].Title;
+		}
+
+		protected override void OnCurrentPageChanged()
+		{
+			base.OnCurrentPageChanged();
+			Title = CurrentPage?.Title ?? string.Empty;
+		}
+	}
+}
